ee5583b257beeca1b398f43bc738d360eb60b436
raxkson <raxkson@gmail.com>
2020-07-17 13:29:11 +0900

    rename check_id to user_info

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, jsonify, request
 from flask_request_validator import (Param, JSON, GET, Pattern, validate_params)
 from db_manager import DBMannager
 
-
 app = Flask(__name__)
 mongo = DBMannager()
+
 
 @app.route('/')
 def hello_world():
@@ -15,10 +15,10 @@ def hello_world():
 def login():
     user_id = request.json['id']
     user_pwd = request.json['pwd']
-    check_id = mongo.get_user_info(user_id)
 
-    if check_id is not None:
-        if user_pwd == check_id['password']:
+    user_info = mongo.get_user_info(user_id)
+    if user_info is not None:
+        if user_pwd == user_info['password']:
             json_request = {'login': 'True'}
         else:
             json_request = {'login': 'False'}
