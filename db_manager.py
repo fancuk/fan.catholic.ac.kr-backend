b1f6b860d90e394572c88504c0fe2f7db2644e64
raxkson <raxkson@gmail.com>
2020-08-27 18:38:10 +0900

    Edit words and Fromat

--- db_manager.py
+++ db_manager.py
@@ -8,7 +8,7 @@ class DBManager(object):
         self.collection = self.db.collection_fancuk
 
     def get_user_info(self, user_id):
-        return self.collection.member.find_one({"user_id": user_id})
+        return self.collection.member.find_one({'user_id': user_id})
 
     def add_user_info(self, json_request):
         return self.collection.member.insert_one({'user_id': json_request[0],
@@ -26,8 +26,8 @@ class DBManager(object):
                                                  {'$set': {'user_pwd': user_pwd}})
 
     def add_library(self, json_request):
-        return self.collection.library.insert_one({"title": json_request[0], "writer": json_request[1],
-                                                   "count": json_request[2], "renter": [], "image": json_request[3]})
+        return self.collection.library.insert_one({'title': json_request[0], 'writer': json_request[1],
+                                                   'count': json_request[2], 'renter': [], 'image': json_request[3]})
 
     def get_library(self):
         return self.collection.library.find()
@@ -35,7 +35,7 @@ class DBManager(object):
     def rent_library(self, title, renter, date, count):
         return self.collection.library.update_one({'title': title},
                                                   {'$push': {'renter': {'user_id': renter, 'date': date}},
-                                                   '$set': {'count': count-1}})
+                                                   '$set': {'count': count - 1}})
 
     def delete_library(self, title):
         return self.collection.library.delete_one({'title': title})
@@ -46,7 +46,7 @@ class DBManager(object):
     def return_library(self, title, renter, count):
         return self.collection.library.update_one({'title': title},
                                                   {'$pull': {'renter': {'user_id': renter}},
-                                                   '$set': {'count': count+1}})
+                                                   '$set': {'count': count + 1}})
 
     def edit_library(self, json_request):
         return self.collection.library.update_one({'title': json_request[0]},
@@ -57,15 +57,15 @@ class DBManager(object):
 
     def edit_user_profile(self, json_request):
         return self.collection.member.update_one({'user_id': json_request[0]},
-                                            {'$set':
-                                                 {'user_pwd': json_request[1],
-                                                  'name': json_request[2],
-                                                  'student_id': json_request[3],
-                                                  'grade': json_request[4],
-                                                  'semester': json_request[5],
-                                                  'phone': json_request[6],
-                                                  'email': json_request[7]
-                                                  }})
+                                                 {'$set':
+                                                      {'user_pwd': json_request[1],
+                                                       'name': json_request[2],
+                                                       'student_id': json_request[3],
+                                                       'grade': json_request[4],
+                                                       'semester': json_request[5],
+                                                       'phone': json_request[6],
+                                                       'email': json_request[7]
+                                                       }})
 
     def get_user_library(self, user_id):
         return self.collection.library.find({'renter': {'$elemMatch': {'user_id': user_id}}})
@@ -76,11 +76,10 @@ class DBManager(object):
     def edit_user_level(self, user_id, level):
         return self.collection.member.update_one({'user_id': user_id},
                                                  {'$set':
-                                                      {
-                                                          'level': level
-                                                      }
-
-        })
+                                                     {
+                                                         'level': level
+                                                     }
+                                                 })
 
     def __del__(self):
         pass
